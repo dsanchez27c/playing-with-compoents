d698a4c763176e76bd5711ebf1f3641d3ed12d34
danielsanchez <danielalejandrosc96@gmail.com>
2024-01-27 15:56:58 -0500

    :lipstick: Adjustment TwitterCard.

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import './../../styles/components/loadingStyle/loadingStyles.css'
+import React from "react";
+import "./../../styles/components/loadingStyle/loadingStyles.css";
 
 function Loading() {
   return (
-    <article className='loader-container'>
-
-        <section className='loader'></section>
+    <article className="loader-container">
+      <section className="loader"></section>
     </article>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
