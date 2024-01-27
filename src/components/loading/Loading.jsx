dbd092b8cac1d665694c548adcb486ab7d9a4c47
danielsanchez <danielalejandrosc96@gmail.com>
2024-01-27 15:56:31 -0500

    :lipstick: Adjustment TwitterCard.

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './../../styles/components/loadingStyle/loadingStyles.css'
+
+function Loading() {
+  return (
+    <article className='loader-container'>
+
+        <section className='loader'></section>
+    </article>
+  )
+}
+
+export default Loading
--- src/components/twitterCard/TwitterCard.jsx
+++ src/components/twitterCard/TwitterCard.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import "./../../styles/components/twitterCardStyle/TwitterCardStyles.css";
+import Loading from "../loading/loading";
 
-export function TwitterCard({userName="", name="", follow, followingYou}) {
-  const [isFollowing, setIsFollowing] = useState(follow)
+export function TwitterCard({
+  userName = "",
+  name = "",
+  follow,
+  followingYou,
+}) {
+  const [isFollowing, setIsFollowing] = useState(follow);
+  const [loadingFollow, setLoadingFollow] = useState(false);
 
-  const imageSrc = `https://unavatar.io/${userName}`
-  const btnText = isFollowing ? "Siguiendo" : "Seguir"
-  const btnClassName = isFollowing ? "twitter-card-btn is-following" : "twitter-card-btn"
+  const imageSrc = `https://unavatar.io/${userName}`;
+  const btnText = isFollowing ? "Siguiendo" : "Seguir";
+  const btnClassName = isFollowing
+    ? "twitter-card-btn is-following"
+    : "twitter-card-btn";
 
   const handleClick = () => {
-    setIsFollowing(!isFollowing)
-  }
+    setLoadingFollow(true);
+    setTimeout(() => {
+      setIsFollowing(!isFollowing);
+      setLoadingFollow(false);
+    }, 170);
+  };
 
   return (
     <article className="twitter-card-container">
@@ -20,18 +33,20 @@ export function TwitterCard({userName="", name="", follow, followingYou}) {
           <strong className="twitter-card-nameuser">{name}</strong>
           <section>
             <span className="twitter-card-name">@{userName}</span>
-            {followingYou && 
-            
-            <span className="twitter-card-follow-status">Te sigue</span>
-            }
+            {followingYou && (
+              <span className="twitter-card-follow-status">Te sigue</span>
+            )}
           </section>
         </div>
       </header>
 
       <aside className="twitter-card-btn-container">
-        <button className={btnClassName} onClick={handleClick}>{btnText}</button>
+        <button className={btnClassName} onClick={handleClick}>
+          <span className="twitter-card-btn-is-following">{btnText}</span>
+          <span className="twitter-card-btn-stop-follow">Dejar de Seguir</span>
+          {loadingFollow && <Loading />}
+        </button>
       </aside>
     </article>
   );
 }
-
