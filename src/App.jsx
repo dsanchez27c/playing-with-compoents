85b63c297d240962f8f302513ae3d75de44475b4
danielsanchez <danielalejandrosc96@gmail.com>
2024-01-29 12:05:54 -0500

    :sparkles: New hooks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import ItemList from "./components/itemList/ItemList";
 import WhoToFollow from "./components/twitterCard/WhoToFollow";
+import { useSEO } from "./hooks/seo/useSEO";
 
 function App() {
+  useSEO({
+    title: "Programando Componentes",
+    description:
+      "Este es un pequeño proyecto donde programaré varios componentes de la web.",
+  });
+
   return (
     <main className="main-content">
       <WhoToFollow />
